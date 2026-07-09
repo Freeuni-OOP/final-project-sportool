3d566129db06d97950d1dfe0f8a0fe1ca4dd9c26
aleko-mamukashvili <amamu23@freeuni.edu.ge>
2026-07-09 16:01:39 +0400

    Fix TrainerDao interface methods for coach services

--- backend/src/main/java/dao/TrainerDao.java
+++ backend/src/main/java/dao/TrainerDao.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 public interface TrainerDao {
     boolean addTrainer(Trainer trainer);
-    boolean updateRating (int trainerId,double newRating,int newReviewCount);
+    boolean updateTrainer(Trainer trainer);
+    boolean updateRating(int trainerId, double newRating, int newReviewCount);
     List<Trainer> getAllTrainers();
     Trainer getTrainerById(int id);
+    Trainer getTrainerByUserId(int userId);
 }
